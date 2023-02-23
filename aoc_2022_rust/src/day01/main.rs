587289e3682d11c8c455ec2ed740c837c5a86c25
tobstern <tobiasphilipp11@gmail.com>
2023-02-23 18:14:37 +0100

    starting day01

--- aoc_2022_rust/src/day01/main.rs
+++ aoc_2022_rust/src/day01/main.rs
@@ -3,14 +3,14 @@
 // bring into scope: use ...
 use std::fs
 
-fn read_txt(day) {
+fn read_txt(String: day) -> String {
     // read in puzzle input: dayXX.txt
-    let file_path = String::from("./puzzle_inputs/day{}.txt", day).expect("Could not open the text-file")
+    let file_path = fs::read_to_string("../puzzle_inputs/day{:?}.txt", day).expect("Could not open the text-file")
 }
 
 fn main() {
     let day = String::from("01");
 
     // read in the text-file
-    read_txt
+    let txt = read_txt(day);
 }
