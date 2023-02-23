1f22742a78082278beffbf31f06d83a13c67f07c
tobstern <tobiasphilipp11@gmail.com>
2023-02-23 18:00:27 +0100

    update structure

---
+++ aoc_2022_rust/src/day01/main.rs
@@ -0,0 +1,16 @@
+// Advent of Code solution //
+
+// bring into scope: use ...
+use std::fs
+
+fn read_txt(day) {
+    // read in puzzle input: dayXX.txt
+    let file_path = String::from("./puzzle_inputs/day{}.txt", day).expect("Could not open the text-file")
+}
+
+fn main() {
+    let day = String::from("01");
+
+    // read in the text-file
+    read_txt
+}
